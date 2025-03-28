4ba89a1203a67d1ace54ce996fbc7abca28f029c
kamilaap <kp.herlambang@gmail.com>
2025-03-28 17:36:28 +0700

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Enable client-side routing
   server: {
     host: '0.0.0.0', // Biar bisa diakses dari laptop lain
     port: 5173,      // Bisa diganti kalau ada konflik port
   }
 })
-
