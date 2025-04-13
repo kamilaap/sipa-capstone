6b2fbd4be0e2b470ec086a80bb06e2812b5ffe3f
kamilaap <kp.herlambang@gmail.com>
2025-04-13 13:53:46 +0700

    integrasi pelayanan lagi 2

--- src/pages/Pelayanan.tsx
+++ src/pages/Pelayanan.tsx
@@ -81,7 +81,7 @@ const Pelayanan = () => {
     
     try {
       // ini api yang sebenarnya
-      const response = await axios.post('https://sipa-chat-production.up.railway.app/ask-model', {
+      const response = await axios.post('https://api-sipa-capstone-production.up.railway.app/ask-model', {
       //ini api mode dev, cek di vite.config.ts
       // const response = await axios.post('/ask', {
         question: userInput
